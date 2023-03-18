8733aef9ce0a0fb629aa05ded78ab368632668d9
jasper <2190320413@qq.com>
2023-03-18 12:58:56 +0800

    init commit

---
+++ demo01/src/com/StackStructureTest.java
@@ -0,0 +1,14 @@
+package com;
+
+public class StackStructureTest {
+    public static void main(String[] args) {
+        int i =2;
+        int j =3;
+        int k=i+j;
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
